a4480e05910b631624cc16b15263c80722512817
Aaron Valera <aaronrvalera@gmail.com>
2026-08-01 13:15:06 -0400

    feature(project): add workspace settings, dependency metadata, and application wiring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfessorsModule } from './professors/professors.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SubjectsModule } from './subjects/subjects.module';
+import { StudentsModule } from './students/students.module';
+import { GradesModule } from './grades/grades.module';
+import { SectionsModule } from './sections/sections.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        uri: ConfigService.get<string>('MONGO_URI'),
+      }),
+    }),
+
+    ProfessorsModule,
+
+    SubjectsModule,
+
+    StudentsModule,
+
+    GradesModule,
+
+    SectionsModule,
+
+    SeedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
